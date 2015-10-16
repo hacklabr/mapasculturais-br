52b7a1efbd91c703384b9f42d1b143c24e921734
Elvis Martins <elvisbmartins@gmail.com>
2015-10-16 13:57:06 -0300

    Logo Mapas Header Save

---
+++ layouts/parts/editable-entity-logo.php
@@ -0,0 +1,7 @@
+<h1 id="small-brand">
+    <a href="<?php echo $app->getBaseUrl() ?>">
+        <img src="<?php $this->asset('img/logo-mapas-culturais-br.svg'); ?>" class="logo-sniic"/>
+        <div class="no-desktop"> Mapas<span>.cultura.gov.br</span></div>
+        <div class="no-mobile"> Mapas<span>.cultura</span></div>
+    </a>
+</h1>
