90626b91228d312c4bd7faec443ffe36ceb67083
Leo Germani <leogermani@gmail.com>
2015-10-16 15:45:20 -0300

    modificando link de ajuda

--- layouts/parts/header-about-nav.php
+++ layouts/parts/header-about-nav.php
@@ -1,7 +1,11 @@
 <nav id="about-nav" class="alignright clearfix">
     <h1 id="organization-logo"><a href="#"><img src="<?php $this->asset('img/logo-sniic.svg'); ?>" /></a></h1>
     <ul id="secondary-menu">
+        <!--
         <li><a class="icon icon-about hltip" href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>" title="Sobre os Mapas Culturais"></a></li>
-        <li><a class="icon icon-help hltip" href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>" title="Como usar"></a></li>
+        -->
+        
+        <li><a class="icon icon-help hltip" href="http://i.cultura.gov.br/novo-cadastro-sniic/" title="Como usar"></a></li>
+		
     </ul>
 </nav>
