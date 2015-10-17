236b31093e5e5fe51a79b78be7004f8c6857cd32
Leo Germani <leogermani@gmail.com>
2015-10-17 02:16:36 -0300

    site title

--- conf-base.php
+++ conf-base.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'app.siteName' => 'Mapas Culturais do Brasil',
-    'app.siteDescription' => 'Mapas Culturais do Brasil'
+    'app.siteName' => 'Mapa da Cultura',
+    'app.siteDescription' => 'Mapas.cultura.gov.br'
 ];
